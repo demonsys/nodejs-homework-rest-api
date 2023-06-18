f33b2197c035ede516e9d1fadd397d2cc06f5628
demonsys <112822206+demonsys@users.noreply.github.com>
2023-06-18 19:44:55 +0300

    created REST API

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,14 @@
-const express = require('express')
+const express = require('express');
+const ctrl = require('../../controllers/contacts');
+const { validateBody } = require('../../middlewares');
+const schemas = require('../../schemas/contacts');
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/', ctrl.listContacts);
+router.get('/:contactId', ctrl.getContactById);
+router.post('/', validateBody(schemas.addContactSchema), ctrl.addContact);
+router.delete('/:contactId', ctrl.removeContact);
+router.put('/:contactId', validateBody(schemas.addContactSchema), ctrl.updateContact);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
