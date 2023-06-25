e5bd6fa552a5b3fd6a377388a6bb7e013251b24f
demonsys <112822206+demonsys@users.noreply.github.com>
2023-06-25 23:01:57 +0300

    connect to MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require('./app');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const DB_HOST =
+  'mongodb+srv://demonsys:uUrFC9HOa5KQpfkg@cluster0.wzajazq.mongodb.net/contacts-db?retryWrites=true&w=majority';
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server running. Use our API on port: 3000');
+    });
+    console.log('Database connection successful');
+  })
+  .catch(error => {
+    console.log(`Server not running. Error message: ${error.message}`);
+    process.exit(1);
+  });
